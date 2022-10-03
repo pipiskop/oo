4a8d3bcbc61d8a7f9fe53ed2a26b194b7ebe7017
pipiskop <isip_s.a.krapovnicky@mpt.ru>
2022-10-03 22:54:08 +0300

    я пытаюсь понять как работает это дерьмо уже 20 минут

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Security.Cryptography;
+Console.WriteLine("Введите номер действия: ");
+Console.WriteLine("1. Сложить");
+Console.WriteLine("2. Вычесть");
+Console.WriteLine("3. Умножить");
+Console.WriteLine("4. Разделить");
+Console.WriteLine("5. Возвести в степень N первое число");
+Console.WriteLine("6. Найти квадратный корень из числа");
+Console.WriteLine("7. Найти 1 процент от числа");
+Console.WriteLine("8. Найти факториал из числа");
+Console.WriteLine("9. Выйти");
+
+Console.Write("Введите первое число: ");
+double NumOdin = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите второе число: ");
+double NumDva = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите номер операции: ");
+double Num = Convert.ToDouble(Console.ReadLine());
+while (Num <= 8)
+{
+    switch (Num)
+    {
+        case 1:
+            Console.Write("ответ: ");
+            Console.WriteLine(NumOdin + NumDva);
+            break;
+        case 2:
+            Console.Write("ответ: ");
+            Console.WriteLine(NumOdin - NumDva);
+            break;
+        case 3:
+            Console.Write("ответ: ");
+            Console.WriteLine(NumOdin * NumDva);
+            break;
+        case 4:
+            Console.Write("ответ: ");
+            Console.WriteLine(NumOdin / NumDva);
+            break;
+        case 5:
+            Console.Write("ответ: ");
+            Console.WriteLine(Math.Pow(NumOdin, NumDva));
+            break;
+        case 6:
+            Console.Write("ответ: ");
+            Console.WriteLine(Math.Sqrt(NumOdin));
+            break;
+        case 7:
+            Console.Write("ответ: ");
+            Console.WriteLine(NumOdin * 0.01);
+            break;
+        case 8:
+
+            int f = 1;
+            int i;
+            for (i = 2; i <= NumDva; i++)
+            {
+                f = f * i;
+            }
+            Console.WriteLine(f);
+            break;
+    }
+    Console.WriteLine("введите операцию: ");
+    Num = Convert.ToDouble(Console.ReadLine());
+}
